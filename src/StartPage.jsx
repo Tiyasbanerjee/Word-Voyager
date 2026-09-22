641b06e868d529c621c85e9a4f3417e27839b0f7
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-22 10:11:48 +0530

    made the startpage basic layout.

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -2,6 +2,15 @@ import styles from './StartPage.module.css';
 
 export default function StartPage(){
     return(
-        <div className={styles.main_box}></div>
+        <div className={styles.main_box}>
+            <div className={styles.settings_box}>
+                <div className={styles.docs_holder}>
+                    Welcome to Word Voyger
+                </div>
+                <div className={styles.controls}>
+
+                </div>
+            </div>
+        </div>
     )
 }
