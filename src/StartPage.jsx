9c4a998d709740fe18cc44d6a490fb37571d785d
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-23 16:45:43 +0530

    make the options and details window in start page arrange ownself in mobile.

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -4,10 +4,10 @@ export default function StartPage(){
     return(
         <div className={styles.main_box}>
             <div className={styles.settings_box}>
-                <div className={styles.docs_holder}>
-                    Welcome to Word Voyger
+                <div className={styles.details}>
+
                 </div>
-                <div className={styles.controls}>
+                <div className={styles.settings}>
 
                 </div>
             </div>
